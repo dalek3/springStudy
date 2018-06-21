41501f874bdd2bbcb383da96acd51f0b8626fc88
lina1318 <lina1318@gmail.com>
2018-06-21 13:24:30 +0900

    ex00 종료

--- ex00/src/main/java/org/zerock/web/SampleController.java
+++ ex00/src/main/java/org/zerock/web/SampleController.java
@@ -8,11 +8,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.ProductVO;
 
 @Controller
 public class SampleController {
 	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+	
+	//
 	
 	@RequestMapping("doA")
 	public void doA(){
@@ -24,12 +28,16 @@ public class SampleController {
 		logger.info("doB called....");
 	}
 	
+	// 
+	
 	@RequestMapping("doC")
 	public String doC(@ModelAttribute("msg") String msg){
 		logger.info("doC called....");
 		return "result";
 	}
 
+	// bean
+	
 	@RequestMapping("doD")
 	public String doD(Model model){
 		logger.info("doD called....");
@@ -37,7 +45,28 @@ public class SampleController {
 		model.addAttribute(product);
 		return "productDetail";
 	}
+	
+	// redirect
+	@RequestMapping("doE")
+	public String doE(RedirectAttributes rttr){
+		logger.info("doE called....");
+		rttr.addFlashAttribute("msg", "redirect msg");
+		return "redirect:/doF";
+	}
+	
+	@RequestMapping("doF")
+	public void doF(@ModelAttribute("msg") String msg){
+		logger.info("doF called...." + msg);
+	}
 
+	// json
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVO doJSON() {
+		ProductVO vo = new ProductVO("sample Product", 10000);
+		return vo;
+	}
+	
+	
 	@RequestMapping("doZ")
 	public String doZ(@RequestParam String msg,ModelMap model){
 		logger.info("doZ called...." + msg);
