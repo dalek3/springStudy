4db53f405ad794a527befeb6f18ec28f879b56dd
lina1318 <lina1318@gmail.com>
2018-06-22 16:37:43 +0900

    ex01-3~8 페이징 처리 dao,비즈니스 계층 까지

--- ex01/src/test/java/org/zerock/controller/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/controller/BoardDAOTest.java
@@ -1,5 +1,7 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
@@ -9,18 +11,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
 		locations = {"classpath:spring/root-context.xml"})
 public class BoardDAOTest {
-/*
+
 	@Inject
 	private BoardDAO dao;
 	
 	private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
-	
+	/*
 	@Test
 	public void testCreate() throws Exception {
 		BoardVO board = new BoardVO();
@@ -32,7 +35,7 @@ public class BoardDAOTest {
 	
 	@Test
 	public void testRead() throws Exception {
-		logger.info(dao.read(2).toString());
+		logger.info(dao.read(3).toString());
 	}
 	
 	@Test
@@ -49,5 +52,31 @@ public class BoardDAOTest {
 	public void testDelete() throws Exception {
 		dao.delete(1);
 	}
+
+	@Test
+	public void testListPage() throws Exception {
+		
+		int page = 3;
+		
+		List<BoardVO> list = dao.listPage(page);
+		
+		for (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + " : " + boardVO.getTitle());
+		}
+	}
 */
+	@Test
+	public void testListCriteria() throws Exception {
+		
+		Criteria cri = new Criteria();
+		cri.setPage(2);
+		cri.setPerPageNum(20);
+		
+		List<BoardVO> list = dao.listCriteria(cri);
+		
+		for (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + " : " + boardVO.getTitle());
+		}
+	}
+	
 }
