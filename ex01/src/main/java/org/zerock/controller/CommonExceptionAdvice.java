c64adc771004843cbb9d8cebbeeacd03307d46a2
dalek3 <lina1318@gmail.com>
2018-06-25 06:57:53 +0900

    ex01 동적sql 앞까지

--- ex01/src/main/java/org/zerock/controller/CommonExceptionAdvice.java
+++ ex01/src/main/java/org/zerock/controller/CommonExceptionAdvice.java
@@ -1,7 +1,7 @@
 package org.zerock.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class CommonExceptionAdvice {
 	
-	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionAdvice.class);
+//	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionAdvice.class);
 	
 //	@ExceptionHandler(Exception.class)
 //	public String common(Exception e) {
--- ex01/src/test/java/org/zerock/controller/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/controller/BoardDAOTest.java
@@ -1,7 +1,5 @@
 package org.zerock.controller;
 
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.junit.Test;
@@ -10,8 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.zerock.domain.BoardVO;
-import org.zerock.domain.Criteria;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -64,7 +62,8 @@ public class BoardDAOTest {
 			logger.info(boardVO.getBno() + " : " + boardVO.getTitle());
 		}
 	}
-*/
+
+	
 	@Test
 	public void testListCriteria() throws Exception {
 		
@@ -78,5 +77,38 @@ public class BoardDAOTest {
 			logger.info(boardVO.getBno() + " : " + boardVO.getTitle());
 		}
 	}
+	*/
 	
+	@Test
+	public void testURI() throws Exception {
+		
+		UriComponents uriComponents = 
+				UriComponentsBuilder.newInstance()
+				.path("/board/read")
+				.queryParam("bno", 12)
+				.queryParam("perPageNum", 20)
+				.build();
+		
+		logger.info("/board/read?bno=12&perPageNum=20");
+		logger.info(uriComponents.toString());
+		
+	}
+
+	@Test
+	public void testURI2() throws Exception {
+		
+		UriComponents uriComponents = 
+				UriComponentsBuilder.newInstance()
+				.path("/{module}/{page}")
+				.queryParam("bno", 12)
+				.queryParam("perPageNum", 20)
+				.build()
+				.expand("board","read")
+				.encode();
+		
+		logger.info("/board/read?bno=12&perPageNum=20");
+		logger.info(uriComponents.toString());
+		
+	}
+
 }
